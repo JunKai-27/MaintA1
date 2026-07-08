bc1042b55b0a45ac830eee64bf76425628321dbf
JunKai-27 <ooijkjks@gmail.com>
2026-07-08 21:08:47 +0800

    MA-2: Create Card Payment file

--- src/CardPayment.java
+++
@@ -1,8 +0,0 @@
-public class CardPayment implements PaymentMethod {
-	private static final double CARD_FEE = 5.0; // To resolve Magic Number in applyFee()
-	
-    @Override
-    public double applyFee(double amount) {
-        return amount + CARD_FEE;
-    }
-}
