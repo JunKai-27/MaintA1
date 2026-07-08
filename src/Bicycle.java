e22afc24e8a8c6597f2671c074a82499a2397517
JunKai-27 <ooijkjks@gmail.com>
2026-07-08 21:39:31 +0800

    Clean up comments in Bicycle.java
    Removed unnecessary comments regarding method overrides.

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -4,6 +4,5 @@ public class Bicycle extends Vehicle {
     	System.out.println("Processing bicycle");
     }
     
-    //No need override startEngine() cuz it does not implement EnginePowered interface.
-    //Also, it is optional to override the move() method since it is a non-abstract method in Vehicle class.
+
 }
